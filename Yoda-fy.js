250af89fb008714c7b07771dd07f6cf618b062a3
Salman Oskooi <soskooi@gmail.com>
2015-12-14 17:12:38 -0600

    design upgrades, text-to-speechfunctionality present but commented out due to unreliable voice option

--- Yoda-fy.js
+++ Yoda-fy.js
@@ -5,13 +5,18 @@ $( "#target" ).submit(function( event ) {
   $.ajax({
     url:"https://yoda.p.mashape.com/yoda?sentence="+phrase,
     headers: {"X-Mashape-Key": "Vs6AqovCQ2msh3xrMJLgHUYnSvPXp17ZFcJjsnlqUlWYcq3SS9", "Accept": "text/plain"},
-    success: function(data){
-           console.log(data)
-           // $('.container').append(JSON.stringify(data))
+    success: function(data) {
+            console.log(data);
+            $(".yoda").text('');
+            $(".form-control").val('');
             $(".yoda").append(data);
+            // var msg = new SpeechSynthesisUtterance(data);
+            // var voices = speechSynthesis.getVoices();
+            // msg.voice = voices[21];
+            // speechSynthesis.speak(msg);
           },
-    error:function(data){
-            console.log(data)
+    error: function(data) {
+            console.log(data);
           }
   })
 })
